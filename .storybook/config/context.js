fa827a04519a04c8c9503605b6f514066d662284
Pierre <pierre.duverneix@gmail.com>
2021-06-25 17:16:52 +0200

    fix sb context json

--- .storybook/config/context.js
+++ .storybook/config/context.js
@@ -8,30 +8,24 @@ export const defaultContext = {
             correctionCode: "Correction filler",
             answers: [
                 {
-                    answer: {
-                        text: "Answer 1",
-                        bestAnswer: false,
-                        tipsAndFeedback: {
-                            tip: "<p>Tip answer 1</p>"
-                        }
+                    text: "Answer 1",
+                    bestAnswer: false,
+                    tipsAndFeedback: {
+                        tip: "<p>Tip answer 1</p>"
                     }
                 },
                 {
-                    answer: {
-                        text: "Answer 2",
-                        bestAnswer: true,
-                        tipsAndFeedback: {
-                            tip: "<p>Tip answer 2</p>"
-                        }
+                    text: "Answer 2",
+                    bestAnswer: true,
+                    tipsAndFeedback: {
+                        tip: "<p>Tip answer 2</p>"
                     }
                 },
                 {
-                    answer: {
-                        text: "Answer 3",
-                        bestAnswer: false,
-                        tipsAndFeedback: {
-                            tip: "<p>Tip answer 3</p>"
-                        }
+                    text: "Answer 3",
+                    bestAnswer: false,
+                    tipsAndFeedback: {
+                        tip: "<p>Tip answer 3</p>"
                     }
                 }
             ]
