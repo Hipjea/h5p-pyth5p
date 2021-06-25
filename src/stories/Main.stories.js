610d9806c6eff68d8a22fce7b4ad95a9f8c23992
Pierre <pierre.duverneix@gmail.com>
2021-06-25 18:11:32 +0200

    Main story

---
+++ src/stories/Main.stories.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import Main from '../components/Main';
+import Snippet from '../components/Snippet';
+import { defaultContext } from '../../.storybook/config/context';
+import { behaviourUneditable, behaviourManual } from '../../.storybook/config/behaviourContext';
+import { defaultEditorContext, uneditableEditorContext } from '../../.storybook/config/editorContext';
+
+export default {
+  title: 'Scenes/Main',
+  component: Main,
+  subcomponents: { Snippet }
+};
+
+const TemplateDefault = (args) => (
+    <Main {...args}>
+        <Snippet {...Default.args} />
+    </Main>
+);
+
+export const Default = TemplateDefault.bind({});
+Default.args = { ...defaultEditorContext, ...defaultContext, ...behaviourManual };
+
+const TemplateUneditable = (args) => (
+    <Main {...args}>
+        <Snippet {...Uneditable.args} />
+    </Main>
+);
+
+export const Uneditable = TemplateUneditable.bind({});
+Uneditable.args = { ...uneditableEditorContext, ...defaultContext, ...behaviourUneditable };
