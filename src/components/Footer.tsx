c70aec086eb8b900dd88ef66cd7118b0ab29c28e
Pierre <pierre.duverneix@gmail.com>
2021-09-17 14:35:47 +0200

    refactor footer tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -11,9 +11,8 @@ import { createPreservedMarkup } from '../utils/utils';
 import xAPILib from '../utils/xapi';
 import './footer.css';
 
-type Props = {
+export type Props = {
     userCode: string;
-    out: string;
     isCodeRun: boolean;
     performRetry: () => void;
     l10n: L10n;
@@ -21,7 +20,7 @@ type Props = {
     behaviour: Behaviour
 };
 
-export default function Footer({userCode, out, isCodeRun, performRetry, ...props}: Props) {
+export default function Footer({userCode, isCodeRun, performRetry, ...props}: Props) {
     const isExercise = props.contentType.isExercise === true;
 
     const context = usePythonCodeContext(),
