882c4d791a9e6cc15a07f899c013096f154f787c
Pierre <pierre.duverneix@gmail.com>
2021-10-14 10:18:01 +0200

    jest ts

--- jest.config.ts
+++ jest.config.ts
@@ -7,7 +7,7 @@ const config: Config.InitialOptions = {
     testEnvironment: 'jsdom',
     moduleNameMapper: {
         '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-            '<rootDir>/src/fileMock.js',
+            '<rootDir>/src/fileMock.ts',
         '\\.(css|less|scss)$': 'identity-obj-proxy',
     },
     setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
